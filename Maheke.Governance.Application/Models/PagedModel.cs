295e287bbbb717937e4f731a46e684fdb1c9322a
maheke-dev <maheke@proton.me>
2024-09-16 23:15:35 -0300

    Add proposal pagination

---
+++ Maheke.Governance.Application/Models/PagedModel.cs
@@ -0,0 +1,26 @@
+﻿using Maheke.Gov.Application.Extensions;
+using System.Collections.Generic;
+
+namespace Maheke.Gov.Application.Models
+{
+    public class PagedModel<TModel> : IPage
+    {
+        const int MaxPageSize = 20;
+        private int _pageSize;
+        public int PageSize
+        {
+            get => _pageSize;
+            set => _pageSize = (value > MaxPageSize) ? MaxPageSize : value;
+        }
+        public int CurrentPage { get; set; }
+        public int TotalItems { get; set; }
+        public int TotalPages { get; set; }
+        public IList<TModel> Items { get; set; }
+        public IDictionary<LinkedResourceType, LinkedResource> Links { get; set; }
+
+        public PagedModel()
+        {
+            Items = new List<TModel>();
+        }
+    }
+}
