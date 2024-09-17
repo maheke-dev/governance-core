62a3f4c2a2342173aba8661f0d80b96968da885d
maheke-dev <maheke@proton.me>
2024-09-16 23:16:11 -0300

    Create ListResponseDto.cs

---
+++ Maheke.Governance.Application/Dtos/ListResponseDto.cs
@@ -0,0 +1,15 @@
+﻿using Maheke.Gov.Application.Extensions;
+using Maheke.Gov.Application.Proposals.Responses;
+using System.Collections.Generic;
+
+namespace Maheke.Gov.Application.Dtos
+{
+    public class ListResponseDto : ILinkedResource
+    {
+        public int CurrentPage { get; set; }
+        public int TotalItems { get; set; }
+        public int TotalPages { get; set; }
+        public List<ProposalIdentifier> Items { get; set; }
+        public IDictionary<LinkedResourceType, LinkedResource> Links { get; set; }
+    }
+}
