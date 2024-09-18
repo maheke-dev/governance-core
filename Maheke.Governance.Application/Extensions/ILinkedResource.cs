ff3860a53520480416ec7ea29f91cbc9b0a7f031
maheke-dev <maheke@proton.me>
2024-09-17 23:36:42 -0300

    Add pagination functionality

---
+++ Maheke.Governance.Application/Extensions/ILinkedResource.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace Maheke.Gov.Application.Extensions
+{
+    public interface ILinkedResource
+    {
+        public IDictionary<LinkedResourceType, LinkedResource> Links { get; set; }
+    }
+}
